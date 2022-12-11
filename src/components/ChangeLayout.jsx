5df3529cd32060f2481fd6062f29e38dfa95c0f4
CodeAlb <infuzzione@gmail.com>
2022-12-11 16:27:24 +0100

    init

--- src/components/ChangeLayout.jsx
+++ src/components/ChangeLayout.jsx
@@ -3,23 +3,33 @@ import usePlayerStore from '../store/store'
 
 export default function ChangeLayout() {
   const setPlayerLayout = usePlayerStore((state) => state.setPlayerLayout)
+  const setSrc = usePlayerStore((state) => state.setSrc)
   return (
-    <div className="change-layouts">
+    <>
+      <div className="change-layouts">
+        <button
+          onClick={() => {
+            setPlayerLayout('horizontal')
+          }}
+        >
+          Horizontal
+        </button>
+        <button
+          onClick={() => {
+            setPlayerLayout('horizontal-reverse')
+          }}
+        >
+          Horizontal Reverse
+        </button>
+        ...etc
+      </div>
       <button
         onClick={() => {
-          setPlayerLayout('horizontal')
+          setSrc('')
         }}
       >
-        Horizontal
+        Stop player
       </button>
-      <button
-        onClick={() => {
-          setPlayerLayout('horizontal-reverse')
-        }}
-      >
-        Horizontal Reverse
-      </button>
-      ...etc
-    </div>
+    </>
   )
 }
